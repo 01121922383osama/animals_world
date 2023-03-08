08a4c4363fd523f1c3639341e0e76565d384f286
01121922383osama <nasamt2002@gmail.com>
2023-03-08 13:59:16 +0200

    ends

--- lib/starting_page.dart
+++ lib/starting_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: library_private_types_in_public_api
+// ignore_for_file: library_private_types_in_public_api, sized_box_for_whitespace
 
 import 'dart:async';
 import 'package:full_screen_image_null_safe/full_screen_image_null_safe.dart';
